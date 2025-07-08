46c391d1a56212978b80177f6a540e906dc2b09c
JoeYunHa <12201806@inha.edu>
2025-07-08 15:35:29 +0900

    login component implemented

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
   server: {
     port: 3000, // 원하는 포트 번호로 변경
   },
+  proxy: {
+    '/api': {
+      target: 'http://localhost:8080', // 스프링부트 서버 주소
+      changeOrigin: true,
+      secure: false,
+    }
+  },
 });
