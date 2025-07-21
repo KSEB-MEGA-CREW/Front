c1668778378d37c336cf5a1a1a4d5bda371bce5d
JoeYunHa <12201806@inha.edu>
2025-07-21 15:56:54 +0900

    modify root

--- src/router/root.jsx
+++ src/router/root.jsx
@@ -4,7 +4,52 @@ import StudyRouter from "./studyRouter";
 import TranslateRouter from "./translateRouter";
 
 // Lazy Loading => 직접 import로 변경
-import Main from "../"
+import Main from "../pages/basicPage/mainPage";
+import Translate from "../pages/translatePage/translatePage";
+import Study from "../pages/studyPage/studyPage";
+import MyPage from "../pages/basicPage/myPage";
+import Login from "../pages/loginPage/loginPage";
+import SignUp from "../pages/loginPage/signupPage";
+import OAuth2RedirectHandler from "../pages/loginPage/OAuth2RedirectHandler";
+import AuthCallback from "../components/authCallback";
 
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/signup",
+    element: <SignUp />
+  },
+  // 기존 OAuth2 리다이렉트 핸들러 (호환성 유지)
+  {
+    path: "/oauth2/redirect",
+    element: <OAuth2RedirectHandler />
+  },
+  // 백엔드 SuccessHandler가 리다이렉트하는 경로
+  {
+    path: "/auth/callback",
+    element: <AuthCallback />
+  },
+  {
+   path: "/",
+   element: (
+     <ProtectedRoute>
+       <Main />
+     </ProtectedRoute>
+   ),
+ },
+ {
+   path: "/mypage",
+   element: (
+     <ProtectedRoute>
+       <MyPage />
+     </ProtectedRoute>
+   ),
+ },
+ TranslateRouter(),
+ StudyRouter(),
+]);
 
 export default router;
